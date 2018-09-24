0fb42b4d3103d483874ac9aa1e77289a40480855
wansong <wansong@sina.cn>
2018-09-24 15:56:13 +0800

    fix ./docker-install

--- docker-install-17.03.sh
+++ docker-install-17.03.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+if [ $# -lt 1 ] ; then
+	echo "Usage: $0 <user_name_to_add_to_docker_group>"
+	exit 255
+fi
+
+target_user=$1
+
 set -e
 
 if [ `id -u` -ne 0 ] ; then
@@ -7,15 +14,18 @@ if [ `id -u` -ne 0 ] ; then
   exit 255
 fi
 
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+if ! which docker >/dev/null ; then
+	apt-get update
+	apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+	add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+	apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+
+fi
 
 groupadd docker 2>/dev/null || printf ''
-if (! groups | grep docker >/dev/null) ; then
-	echo "user: $USER not in docker group, enrolling .."
-	usermod -aG docker $USER
+if ! groups $target_user | grep docker >/dev/null ; then
+	echo "user: $target_user not in docker group, enrolling .."
+	usermod -aG docker $target_user
 fi
 
